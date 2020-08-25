173b1c0f2a2efdb5dc402eb272b346201cc770b0
judowha <e0376925@u.nus.edu>
2020-08-25 18:14:24 +0800

    week3 level 1

--- src/main/java/my_bot.java
+++ src/main/java/my_bot.java
@@ -1,8 +1,16 @@
-public class level0 {
+import java.util.Scanner;
+
+public class my_bot {
     public static void main(String[] args){
+
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("");
+        String line;
+        Scanner in=new Scanner(System.in);
+        while(!(line= in.nextLine()).equals("bye")){
+            System.out.println("   "+line);
+        }
         System.out.println("Bye. Hope to see you again soon");
     }
 
