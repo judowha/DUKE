0b308145fbc930ba0c65ff09622cebfb4ea1c430
judowha <e0376925@u.nus.edu>
2020-08-25 18:28:46 +0800

    week 3 level2: add and store inputs

--- src/main/java/my_bot.java
+++ src/main/java/my_bot.java
@@ -6,12 +6,27 @@ public class my_bot {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("");
+        storeInput();
+        System.out.println("Bye. Hope to see you again soon");
+    }
+    public static void storeInput(){
         String line;
+        String[] list=new String[100];
+        int listNUm=0;
         Scanner in=new Scanner(System.in);
         while(!(line= in.nextLine()).equals("bye")){
-            System.out.println("   "+line);
+            if(line.equals("list")){
+                for(int i=0;i<listNUm;i++){
+                    System.out.println((i+1)+"."+list[i]);
+                }
+            }
+            else{
+                list[listNUm]=line;
+                System.out.println("   added: "+line);
+                listNUm++;
+            }
         }
-        System.out.println("Bye. Hope to see you again soon");
+
     }
 
 }
